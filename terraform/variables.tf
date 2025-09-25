df476ee65ca81da2a984433074b944b74ecd4bab
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-25 13:47:59 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Name of the AWS key pair"
+}
+
+variable "github_username" {
+  description = "Your GitHub username"
+}
+
+variable "github_repo" {
+  description = "Your GitHub repository name"
+}
