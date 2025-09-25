23e4e9cfdfd87632a3b36fbae671a2f6c64f0fde
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-25 19:20:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,48 +3,50 @@ provider "aws" {
 }
 
 resource "aws_instance" "react_app" {
-  ami           = "ami-08c40ec9ead489470" # Ubuntu 22.04 (check region)
+  ami           = "ami-08c40ec9ead489470" # Ubuntu 22.04
   instance_type = "t2.micro"
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.react_sg.id]
 
-#!/bin/bash
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+  user_data = <<-EOF
+    #!/bin/bash
+    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-echo "===== USER DATA STARTED ====="
+    echo "===== USER DATA STARTED ====="
 
-# Update & install basics
-apt-get update -y
-apt-get install -y curl git nginx
+    # Update & install basics
+    apt-get update -y
+    apt-get install -y curl git nginx
 
-# Install Node.js LTS
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs build-essential
+    # Install Node.js LTS
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+    apt-get install -y nodejs build-essential
 
-# Clone repo
-rm -rf /home/ubuntu/reactapp
-git clone ${var.github_repo} /home/ubuntu/reactapp
-cd /home/ubuntu/reactapp
+    # Clone repo
+    rm -rf /home/ubuntu/reactapp
+    git clone ${var.github_repo} /home/ubuntu/reactapp
+    cd /home/ubuntu/reactapp
 
-# Build react app
-npm install
-npm run build
+    # Build react app
+    npm install
+    npm run build
 
-# Copy build files to nginx root
-rm -rf /var/www/html/*
-cp -r build/* /var/www/html/
+    # Copy build files to nginx root
+    rm -rf /var/www/html/*
+    cp -r build/* /var/www/html/
 
-# Fix permissions
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+    # Fix permissions
+    chown -R www-data:www-data /var/www/html
+    chmod -R 755 /var/www/html
 
-# Restart nginx
-sleep 5
-systemctl enable nginx
-systemctl restart nginx
+    # Restart nginx
+    sleep 5
+    systemctl enable nginx
+    systemctl restart nginx
 
-echo "===== USER DATA FINISHED ====="
+    echo "===== USER DATA FINISHED ====="
+  EOF
 
   tags = {
     Name = "react-app-server"
